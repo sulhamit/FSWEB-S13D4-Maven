d4bf2fe9b82a486cf252e436e27ddc92188529e2
sulhamit <sulhamit@gmail.com>
2025-11-01 09:23:22 +0100

    first mission has been completed

---
+++ src/main/java/org/example/Point.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+    public int getX(){
+        return this.x;
+    }
+    public int getY(){
+        return this.y;
+    }
+    public void setX(int x){
+        this.x=x;
+    }
+    public void setY(int y){
+        this.y=y;
+    }
+
+    public double distance(){
+return Math.sqrt((x*x)+(y*y));
+    }
+    public double distance(Point p){
+        return Math.sqrt(
+                (p.x - this.x)*(p.x - this.x) + (p.y - this.y)*(p.y - this.y)
+        );
+    }
+    public double distance(int x, int y){
+        return Math.sqrt((x-this.x)*(x-this.x)+(y-this.y)*(y-this.y));
+    }
+}
